620d83d46e4493a3da12dac6fcb74d170505c6fb
Mario <max_cable@hotmail.com>
2024-01-07 23:11:44 -0600

    add module http-client

---
+++ modules_http-client.js
@@ -0,0 +1,15 @@
+// If module http is used, the default por is 80 and the protocol is http
+// If module https is used, the default por is 443 and the protocol is https
+import { get } from "node:https";
+
+const myURL = {
+  hostname: "letrasnubladas.co",
+  port: 443,
+  path: "/about/"
+};
+
+get(myURL, (res) => {
+  console.log(`The response to ${myURL.hostname} is ${res.statusCode} and ${res.statusMessage}`);
+}).on("error", (err) => {
+  console.error(err);
+});
